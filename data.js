938ffa2b766f9b2e2a9d3bde6105dc565978bb86
gyanpatra <gyan101@gmail.com>
2014-07-23 21:39:39 -0700

    Initial BollyMoods commit

---
+++ data.js
@@ -0,0 +1,19 @@
+/**
+ * New node file
+ */
+var data = [
+    {
+		title: 'Video Title',
+		description: 'Description of the video',
+		video: src='http://www.youtube.com/embed/M2WTRqCu_DY'
+		
+	},
+	{
+		title: 'Video Title 2', 
+		description: 'Description of the video 2',
+		url: 'https://www.youtube.com/watch?v=M4EZHGFK-1c',
+		footer: 'Video footer 2'	
+	}
+];
+
+exports.data = data;
---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var data = require('../data');
+
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+  res.render('index', { 
+	/*  title: 'Express1', 
+	  comment: "Joe smells funny 2", */
+	  foo: {bar: "bar", baz: "baz"},	  
+	  posts: data
+  });
+};
